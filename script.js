1202fe7baaec8a8df9186b595f91f465f18d03ea
maisaaamorim <maisa.aamorim987@gmail.com>
2022-06-29 21:28:28 -0300

    versao inicial

---
+++ script.js
@@ -0,0 +1,37 @@
+const nome = "Maisa"
+const sobrenome = "Amorim"
+const data_nacimento = "06/03/2006"
+
+let altura = 1.66
+let numero_sapato = 38
+let idade = 16
+
+let celular = "(18) 99680-1567"
+let cor_favorita = "preto"
+
+let qtde_moto = null
+let qtde_carro = null
+
+// <- o valor
+
+qtde_carro = 1
+
+altura = 1.67
+
+celular = "(18) 00000-0000"
+
+// comparar/validar/verificar igualdade
+
+nome.trim().toUpperCase() == 17 //true
+
+idade == 15 //true
+
+idade == 17
+
+idade == 15 //true
+
+(true == false) == (true == false)
+
+//igualdade
+
+nome === "maisa"
